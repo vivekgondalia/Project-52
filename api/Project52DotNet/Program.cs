19d749ada424cb4cedeca31ea370849f58ae59ab
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-26 16:23:22 -0700

    Register the DB(in memory) - using Dependency Injection

--- api/Project52DotNet/Program.cs
+++ api/Project52DotNet/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Project52DotNet.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services to the container
 builder.Services.AddControllers();
+builder.Services.AddDbContext<MovieContext>(opt =>
+    opt.UseInMemoryDatabase("Project52"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
