6affccc7bd4c85a26edbfddbdaa414ae6da8f0f3
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-05-26 16:15:28 -0700

    Initiated NET Web API project and created a MovieItem data model

---
+++ api/Project52DotNet/Models/MovieItem.cs
@@ -0,0 +1,9 @@
+namespace Project52DotNet.Models;
+
+public class MovieItem
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public int Year { get; set; }
+    public int Month {get; set;}
+}
