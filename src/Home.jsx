315bd3b304a0ca6d3464135fd102760901b129ec
Vivek Gondalia <hire.vivekgondalia@gmail.com>
2024-06-02 09:40:49 -0700

    Backend - Get list of Movies for the given month

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,8 @@
 const Home = () => {
-    return <h1>{} Movies</h1>
+    return (
+        <h1>Movies of the Month</h1>
+
+    );
 }
 
 export default Home;
---
+++ src/HomeMovieTable.jsx
@@ -0,0 +1,6 @@
+const HomeMovieTable = () => {
+    console.log("Create an endpoint to fetch ONLY THIS MONTH MOVIES");
+    return "Movie Table"
+}
+
+export default HomeMovieTable;
